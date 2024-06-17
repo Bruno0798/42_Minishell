0366824b93169c6bc1a3699ff03a62ad6c253f6b
Bruno0798 <brunomiguel0798@gmail.com>
2024-06-17 12:20:25 +0100

    fixing expand, exit code its done, still missing the normal expands

--- srcs/utils/expander.c
+++ srcs/utils/expander.c
@@ -6,133 +6,84 @@
 /*   By: brpereir <brpereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:09:50 by bsousa-d          #+#    #+#             */
-/*   Updated: 2024/05/27 17:44:57 by bsousa-d         ###   ########.fr       */
+/*   Updated: 2024/06/13 12:42:27 by bsousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char *expand_variable(char *input, int index, t_commands *command) {
-	char *key;
-	char *value;
-	char *num;
+char *expand_exit_code(char *string);
 
-	if (input[index] == '$' && input[index + 1] == '?') {
-		num = ft_itoa(EXIT_STATUS);
-		input = replace_substr(input, "$?", num, index);
-		free(num);
-	} else {
-		key = trim_right(&input[index]);
-		value = ft_get_value(command->env, key + 1);
-		if (value)
-			input = replace_substr(input, key, value, index);
-		else
-			input = replace_substr(input, key, "", index);
-		free(key);
-	}
-	return input;
-}
+void ft_expander(t_commands *commands)
+{
+	t_token *token = commands->token;
 
-char *needs_expansion(char *input, char c, t_commands *command) {
-	bool single_quotes = false;
-	bool double_quotes = false;
-	int i = 0;
-	int length;
+	EXIT_STATUS = 4000000;
 
-	while (input[i]) {
-		if (input[i] == '\'' && !double_quotes) {
-			single_quotes = !single_quotes;
-		} else if (input[i] == '"' && !single_quotes) {
-			double_quotes = !double_quotes;
-		} else if (input[i] == c && !single_quotes) {
-			length = strlen(input);
-			input = expand_variable(input, i, command);
-			i += strlen(input) - length;
-		}
-		if (i > ft_strlen(input))
-			break ;
-		i++;
-	}
-	return input;
-}
-
-void ft_expander(t_commands *command) {
-	t_token *token = command->token;
-
-	while (token != NULL) {
-		if (ft_strchr(token->content, '$') && *(ft_strchr(token->content, '$') + 1) != '\0'){
-			token->content = needs_expansion(token->content, '$', command);
+	while (token)
+	{
+		if(ft_strchr(token->content, '$') && *(ft_strchr(token->content, '$') + 1) != '\0')
+		{
+			if(*(ft_strchr(token->content, '$') + 1) == '?')
+				token->content = expand_exit_code(token->content);
 		}
 		token = token->next;
 	}
 }
 
-char *replace_substr(char *original, const char *substr, char *replacement, int i) {
-	if (!original || !substr || !replacement) {
-		return NULL;
+int calculate_extra_length(char *string, int num_len)
+{
+	int i;
+	int extra_length;
+
+	i = 0;
+	extra_length = 0;
+	while (string[i]) {
+		if (string[i] == '$' && string[i + 1] == '?') {
+			extra_length += num_len - 2;
+			i++;
+		}
+		i++;
 	}
-	size_t original_len = ft_strlen(original);
-	size_t substr_len = ft_strlen(substr);
-	size_t replacement_len = ft_strlen(replacement);
-
-	// Find the position of the substring in the original string
-	const char *substr_pos = ft_strnstr(original + i, substr,
-										ft_strlen(original));
-	if (!substr_pos) {
-		return NULL; // Substring not found
-	}
-
-	// Calculate the length of the resulting string
-	size_t result_len = original_len - substr_len + replacement_len;
-
-	// Allocate memory for the resulting string
-	char *result = malloc(result_len + 1);
-	if (!result) {
-		return NULL;
-	}
-
-	// Copy the part of the original string before the substring
-	strncpy(result, original, substr_pos - original);
-	result[substr_pos - original] = '\0';
-
-	// Concatenate the replacement string
-	strcat(result, replacement);
-
-	// Concatenate the part of the original string after the substring
-	strcat(result, substr_pos + substr_len);
-	free(original);
-
-	return result;
+	return extra_length;
 }
 
-char *trim_right(const char *input_string) {
-	const char *ptr = input_string;
-	char *result;
-	int length = 0;
+void replace_exit_code(char *string, char *new_string, char *num, int num_len) {
+	int i;
+	int j;
+	int k;
 
-	// Skip initial spaces
-	while (*ptr && isspace(*ptr)) {
-		ptr++;
+	i = 0;
+	k = 0;
+	while (string[i]) {
+		if (string[i] == '$' && string[i + 1] == '?') {
+			j = 0;
+			while (j < num_len) {
+				new_string[k++] = num[j++];
+			}
+			i += 2; // Skip the "$?"
+		} else {
+			new_string[k++] = string[i++];
+		}
 	}
+	new_string[k] = '\0';
+}
 
-	// Find the end of the first word
-	while (*ptr && !isspace(*ptr) && *ptr != '\'' && *ptr != '"' && *ptr != '=') {
-		ptr++;
-		length++;
-	}
+char *expand_exit_code(char *string) {
+	char *num;
+	char *new_string;
+	int num_len;
+	int extra_length;
 
-	// Allocate memory for the result string
-	result = (char *)malloc((length + 1) * sizeof(char));
-	if (result == NULL) {
-		printf("Memory allocation failed\n");
-		exit(1);
-	}
+	num = ft_itoa(EXIT_STATUS);
+	num_len = ft_strlen(num);
+	extra_length = calculate_extra_length(string, num_len);
 
-	// Copy the first word to the result string
-	strncpy(result, input_string, length);
-	result[length] = '\0'; // Null-terminate the result string
+	new_string = malloc(ft_strlen(string) + extra_length + 1);
+	replace_exit_code(string, new_string, num, num_len);
 
-	return result;
+	free(num);
+	return new_string;
 }
 
 char *ft_get_value(t_env *env, char *key) {
